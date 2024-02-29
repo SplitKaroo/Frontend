0ebdf1c2dfae1453655f96365887dd1e38a97f29
Kartik Prajapat <prajapatatwork0712@gmail.com>
2024-02-29 08:07:01 +0530

    removed dotenv

--- src/supabase/utils/supabase.utils.js
+++ src/supabase/utils/supabase.utils.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY)
+
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL,process.env.REACT_APP_SUPABASE_KEY)
 
 export const signInWithGoogle = async () => {
     try {
