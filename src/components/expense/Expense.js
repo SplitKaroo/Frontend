0de50c32390a81aee88e58a70852872f82347169
Kartik Prajapat <prajapatatwork0712@gmail.com>
2024-07-07 00:30:15 +0530

    added Transaction section and started with expense form

--- src/components/expense/Expense.js
+++ src/components/expense/Expense.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Expense() {
-  return <div>Expense</div>;
+export default function Expense(props) {
+  const [isChecked, setIsChecked] = useState(true);
+  return (
+    <div className="expense-container">
+      <div className="expense-form">
+        <form>
+          <input type="text" placeholder="Expense name" />
+          <label>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={(e) => setIsChecked(e.target.checked)}
+            />
+            Did you pay?
+          </label>
+          <input type="text" placeholder="Payed by?" disabled={isChecked} />
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Expense from "../expense/Expense";
 import History from "../history/History";
+import axios from "axios";
 
 export default function Transaction() {
   const [activeComponent, setActiveComponent] = useState("Expense");
+  const [session, setSession] = useState(null);
+  const [friendList, setFriendList] = useState([]);
+
+  useEffect(() => {
+    setSession(JSON.parse(localStorage.getItem("supabase_session")));
+  }, []);
+  useEffect(() => {
+    if (
+      session != null &&
+      session.user != null &&
+      session.user.aud != null &&
+      session.user.aud == "authenticated"
+    ) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${session.access_token}`,
+        },
+      };
+      axios
+        .get("http://localhost:3030/getAllFriend", config)
+        .then((response) => {
+          setFriendList(response.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [session]);
+
   const renderComponent = () => {
     switch (activeComponent) {
       case "Expense":
-        return <Expense />;
+        return <Expense groupName="" groupCreator="" memberList={friendList} />;
       case "History":
         return <History />;
     }
